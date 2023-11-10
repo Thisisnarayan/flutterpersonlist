9e5dc4cd8db9999ea901056a855cd55d8648a214
Narayan Sharma <narayansharma1806@gmail.com>
2023-11-11 04:03:01 +0530

    detail page update

--- lib/modules/user_list/details/user_detail.dart
+++ lib/modules/user_list/details/user_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutterpersonlist/modules/user_list/details/user_detail_view_model.dart';
 import 'package:flutterpersonlist/modules/user_list/model/user.dart';
 import 'package:provider/provider.dart';
@@ -28,55 +29,55 @@ class _UserDetailScafflodState extends State<UserDetailScafflod> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('${viewModel.userData.} ${userData['lastname']}'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: NetworkImage(userData['image']),
+        appBar: AppBar(title: Text('hi')),
+        body: Observer(builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 20, // Adjust the radius as needed
+                  // backgroundImage: NetworkImage(user.photoUrl), // Replace with the actual photo URL
+                  child: Text(
+                    viewModel.userData!.firstname![0], // Display the first alphabet
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Email: ${viewModel.userData?.email}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Phone: ${viewModel.userData?.phone}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Birthday: ${viewModel.userData?.birthday}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                // ignore: prefer_const_constructors
+                SizedBox(height: 8),
+                Text(
+                  'Gender: ${viewModel.userData?.gender}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Address:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '${viewModel.userData?.address?.street}, ${viewModel.userData?.address?.city}, ${viewModel.userData?.address?.zipcode}, ${viewModel.userData?.address?.country}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+              ],
             ),
-            SizedBox(height: 16),
-            Text(
-              'Email: ${userData['email']}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Phone: ${userData['phone']}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Birthday: ${userData['birthday']}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Gender: ${userData['gender']}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Address:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '${userData['address']['street']}, ${userData['address']['city']}, ${userData['address']['zipcode']}, ${userData['address']['country']}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Website: ${userData['website']}',
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+        }));
   }
 }
